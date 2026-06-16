9359e77e0adbd747a1a34b0ffa130b4083783ee1
Amaya0623 <isicontrolacces@gmail.com>
2026-06-15 21:02:14 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zeus-cache-v1.62'; // Subimos de versión para limpiar caché antigua
+const CACHE_NAME = 'zeus-cache-v1.63'; // Subimos de versión para limpiar caché antigua
 
 // Archivos críticos para modo Offline y para que el navegador active la instalación
 const urlsToCache = [
