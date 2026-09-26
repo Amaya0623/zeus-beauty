7740f7709e098e5b47ebef28ef5d40493f8eb97e
Amaya0623 <isicontrolacces@gmail.com>
2026-09-26 13:30:51 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zeus-cache-v1.145'; // 🚀 Subimos versión para forzar el Modo Supervivencia
+const CACHE_NAME = 'zeus-cache-v1.146'; // 🚀 Subimos versión para forzar el Modo Supervivencia
 
 // 🛡️ BÓVEDA OFFLINE INICIAL: Solo archivos locales. 
 // Las librerías externas (Tailwind, SweetAlert) se guardarán automáticamente en caché cuando la app las use por primera vez.
